1197fc4f54cb857c0b10c2b037d33d602f6857e6
Olavo Mello <olavo@agenciadix.com.br>
2020-01-17 23:26:31 -0300

    Update all

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const express = require("express");
+
+const app = express();
+
+// Routes
+app.get("/", ( req, res ) => {
+    console.log(req.query);
+    return res.json({ message : "Hello World !"});
+});
+
+app.listen(3333);
