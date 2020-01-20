69f683aa37a175dee3ba50ad620eb549d8bc820e
Olavo Mello <olavo@agenciadix.com.br>
2020-01-19 21:59:07 -0300

    Update Dev Models to MongoDB Data

---
+++ src/models/dev.js
@@ -0,0 +1,32 @@
+﻿const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+
+const DevSchema = new Schema({
+    name : {
+        type: String,
+        required: true,
+        trim: true
+    },
+    github_username : {
+        type: String,
+        unique: false,
+        required: true,
+        trim: true
+    },
+    bio : {
+        type: String,
+        trim: true
+    },
+    avatar_url : {
+        type: String,
+        trim: true
+    },
+    techs : {
+        type: [String],
+        trim: true
+    },
+    created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Dev", DevSchema);
